ac7ff89afaed391615498b6b0182f485da4b3d24
kerkompm2 <kerkompm2@gmail.com>
2024-12-13 06:40:09 +0000

    #3 sign up

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -1,23 +1,18 @@
+import 'dart:ffi';
+import 'package:myapp/app/controllers/auth_controllers.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignupController extends GetxController {
-  //TODO: Implement SignupController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  final cEmail = TextEditingController();
+  final cPass = TextEditingController();
+  final cCpass = TextEditingController();
 
   @override
   void onClose() {
+    cEmail.dispose();
+    cPass.dispose();
+    cCpass.dispose();
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/signup/views/signup_view.dart
+++ lib/app/modules/signup/views/signup_view.dart
@@ -1,22 +1,102 @@
 import 'package:flutter/material.dart';
-
+import 'package:myapp/app/controllers/auth_controllers.dart';
 import 'package:get/get.dart';
 
 import '../controllers/signup_controller.dart';
 
 class SignupView extends GetView<SignupController> {
-  const SignupView({super.key});
+  final cAuth = Get.find<AuthController>();
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SignupView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SignupView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo
+                Image.asset(
+                  'assets/bg.png', // Sesuaikan path logo Anda
+                  height: 100,
+                ),
+                const SizedBox(height: 16),
+                // Title
+                
+                const SizedBox(height: 32),
+                // Email TextField
+                TextField(
+                  controller: controller.cEmail,
+                  decoration: InputDecoration(
+                    hintText: 'Masukan Email',
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Password TextField
+                TextField(
+                  controller: controller.cPass,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Masukan Password',
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                // Confirm Password TextField
+                TextField(
+                  
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Ulangi password',
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                // Register Button
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 0, 91, 26),
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                    onPressed: () {
+                cAuth.signup(
+                  controller.cEmail.text,
+                  controller.cPass.text,
+                );
+              },
+                    child: const Text(
+                      'Register',
+                      style: TextStyle(fontSize: 16, color:Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
